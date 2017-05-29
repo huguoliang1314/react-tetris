9a4ba344a352e58ba97cdc456112f2667a8c868a
guoliang <799268820@qq.com>
2017-05-29 15:28:40 +0800

    '提交'

--- src/component/content/index.jsx
+++ src/component/content/index.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Background from './background.jsx';
 
 export default class Content extends React.Component {
 
-
 	render() {
+	  const rows = [0,1,2,3,4,5,6,7,8,9];
+		const columns = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 		return (
 			<div>
-				<div className="board-content">
-					for (var i = 0; i < 20; i++) {
-						<div className="board-row">
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-							<button className="square"></button>
-						</div>
-					}
-				</div>
+				<Background rows={rows} columns={columns}/>
 			</div>
 		);
 	}
---
+++ src/component/content/tetromino.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default class Tetromino extends React.Component{
+
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
