711c2ad354817b765e7ff83f6248ea7546a1d960
Hu GuoLiang <wb-hgl249036@alibaba-inc.com>
2017-05-29 12:25:10 +0800

    no message

--- src/component/content/index.jsx
+++ src/component/content/index.jsx
@@ -4,28 +4,26 @@ import './index.css';
 
 
 export default class Content extends React.Component {
+
+
 	render() {
 		return (
 			<div>
-				<div>
-					<div className="board-row">
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-					</div>
-					<div className="board-row">
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-					</div>
-					<div className="board-row">
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-						<button className="square"></button>
-					</div>
+				<div className="board-content">
+					for (var i = 0; i < 20; i++) {
+						<div className="board-row">
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+							<button className="square"></button>
+						</div>
+					}
 				</div>
 			</div>
 		);
